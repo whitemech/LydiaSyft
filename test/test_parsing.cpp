4b9e1bc5673c298ea36018659d34677f6450d646
Marco Favorito <marco.favorito@gmail.com>
2023-05-27 22:22:13 +0200

    test: add utils.hpp header, add __ROOT_DIRECTORY definition

--- test/test_parsing.cpp
+++ test/test_parsing.cpp
@@ -1,5 +1,8 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include "Parser.h"
+#include "utils.hpp"
+
 
 TEST_CASE( "Parse TLSF", "[tlsf_parser]" ) {
   REQUIRE(true);
---
+++ test/utils.hpp
@@ -0,0 +1,14 @@
+#ifndef LYDIASYFT_TEST_UTILS_HPP
+#define LYDIASYFT_TEST_UTILS_HPP
+
+#include <string>
+
+namespace Syft::Test {
+
+  static std::string ROOT_DIRECTORY = __ROOT_DIRECTORY;
+  static std::string SYFCO_LOCATION = ROOT_DIRECTORY + "/syfco-bin/syfco";
+  static std::string EXAMPLE_TEST_TLSF = ROOT_DIRECTORY + "/example/test.tlsf";
+
+}
+
+#endif //LYDIASYFT_TEST_UTILS_HPP
