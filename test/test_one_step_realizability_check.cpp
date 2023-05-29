35b3b3c520cfce5fa5d4fc15c13c800338d3f61d
Marco Favorito <marco.favorito@gmail.com>
2023-05-29 17:00:37 +0200

    test: refactor and add more tests on one-step-realizability-check

--- test/test_one_step_realizability_check.cpp
+++ test/test_one_step_realizability_check.cpp
@@ -3,16 +3,46 @@
 #include "OneStepRealizability.h"
 #include "utils.hpp"
 
+
+std::optional<CUDD::BDD> get_one_step_realizability(const std::string& formula, const std::vector<std::string>& input_variables, const std::vector<std::string>& output_variables, Syft::VarMgr& var_mgr, whitemech::lydia::parsers::ltlf::LTLfDriver& driver) {
+    auto parsed_formula = Syft::Test::parse_formula(formula, driver);
+    auto partition = Syft::InputOutputPartition::construct_from_input(input_variables, output_variables);
+    var_mgr.create_named_variables(partition.input_variables);
+    var_mgr.create_named_variables(partition.output_variables);
+    return Syft::one_step_realizable(*parsed_formula, partition, var_mgr);
+}
+
+
 TEST_CASE("One-step realizability check of true", "[one-step-realizability-check]") {
     auto driver = std::make_shared<whitemech::lydia::parsers::ltlf::LTLfDriver>();
-    auto formula = Syft::Test::parse_formula("true", *driver);
-    auto partition = Syft::InputOutputPartition::construct_from_input({}, {});
-    std::shared_ptr<Syft::VarMgr> var_mgr = std::make_shared<Syft::VarMgr>();
-    var_mgr->create_named_variables(partition.input_variables);
-    var_mgr->create_named_variables(partition.output_variables);
-
-    auto actual = Syft::one_step_realizable(*formula, partition, *var_mgr);
+    auto var_mgr = std::make_shared<Syft::VarMgr>();
+    auto actual = get_one_step_realizability("true", vars{}, vars{}, *var_mgr, *driver);
     auto expected = var_mgr->cudd_mgr()->bddOne();
-
     REQUIRE(actual == expected);
+}
+
+TEST_CASE("One-step realizability check of false", "[one-step-realizability-check]") {
+    auto driver = std::make_shared<whitemech::lydia::parsers::ltlf::LTLfDriver>();
+    auto var_mgr = std::make_shared<Syft::VarMgr>();
+    auto actual = get_one_step_realizability("false", vars{}, vars{}, *var_mgr, *driver);
+    auto expected = std::nullopt;
+    REQUIRE(actual == expected);
+}
+
+TEST_CASE("One-step realizability check of a", "[one-step-realizability-check]") {
+    auto driver = std::make_shared<whitemech::lydia::parsers::ltlf::LTLfDriver>();
+    auto var_mgr = std::make_shared<Syft::VarMgr>();
+
+    SECTION("a controllable"){
+        auto actual = get_one_step_realizability("a", vars{}, vars{"a"}, *var_mgr, *driver);
+        auto expected = var_mgr->name_to_variable("a");
+        REQUIRE(actual == expected);
+    }
+
+    SECTION("a uncontrollable"){
+        auto actual = get_one_step_realizability("a", vars{"a"}, vars{}, *var_mgr, *driver);
+        auto expected = std::nullopt;
+        REQUIRE(actual == expected);
+    }
+
 }
