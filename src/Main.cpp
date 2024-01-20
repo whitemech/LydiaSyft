d5748826710c4f76adfbe99adfc6bd4de6fe6b96
Shufang-Zhu <shufang.zhu@cs.ox.ac.uk>
2024-01-20 16:31:58 +0000

    added maxset synthesizer

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,6 +7,7 @@
 #include "parser/Parser.h"
 #include "ExplicitStateDfaMona.h"
 #include "ReachabilitySynthesizer.h"
+#include "ReachabilityMaxSetSynthesizer.h"
 #include "InputOutputPartition.h"
 #include "Preprocessing.h"
 #include <lydia/parser/ltlf/driver.hpp>
@@ -30,6 +31,9 @@ int main(int argc, char ** argv) {
 
     bool print_times = false;
     app.add_flag("-t, --print-times", print_times, "Print out running times of each step (default: false)");
+
+    bool maxset = false;
+    app.add_flag("-m,--maxset", maxset, "Maxset flag (Default: false)");
 
     CLI11_PARSE(app, argc, argv);
     Syft::Stopwatch total_time_stopwatch; // stopwatch for end-to-end execution
@@ -64,77 +68,130 @@ int main(int argc, char ** argv) {
     auto not_end = context->makeLtlfNotEnd();
     parsed_formula = context->makeLtlfAnd({parsed_formula, not_end});
 
-    // preprocessing
-    auto one_step_result = preprocessing(*parsed_formula, partition, *var_mgr);
-    bool preprocessing_success = one_step_result.realizability.has_value();
-    if (preprocessing_success and one_step_result.realizability.value()){
-      std::cout << Syft::REALIZABLE_STR << std::endl;
-      CUDD::BDD move = one_step_result.winning_move;
-      auto total_time = total_time_stopwatch.stop();
-      if (print_strategy){
-          var_mgr->dump_dot(move.Add(), "strategy.dot");
-      }
+    if (!maxset) {
+        // abstract single strategy
+        // preprocessing
+//        auto one_step_result = preprocessing(*parsed_formula, partition, *var_mgr);
+//        bool preprocessing_success = one_step_result.realizability.has_value();
+//        if (preprocessing_success and one_step_result.realizability.value()) {
+//            std::cout << Syft::REALIZABLE_STR << std::endl;
+//
+//            CUDD::BDD move = one_step_result.winning_move;
+//            auto total_time = total_time_stopwatch.stop();
+//            if (print_strategy) {
+//                var_mgr->dump_dot(move.Add(), "strategy.dot");
+//            }
+//
+//            if (print_times) {
+//                std::cout << "Total time: "
+//                          << total_time.count() << " ms" << std::endl;
+//            }
+//            return 0;
+//        } else if (preprocessing_success and !one_step_result.realizability.value()) {
+//            std::cout << Syft::UNREALIZABLE_STR << std::endl;
+//            auto total_time = total_time_stopwatch.stop();
+//
+//            if (print_times) {
+//                std::cout << "Total time: "
+//                          << total_time.count() << " ms" << std::endl;
+//            }
+//            return 0;
+//        } else {
+//            // Preprocessing was not successful. Continuing with full DFA construction."
+//        }
 
-      if (print_times) {
-        std::cout << "Total time: "
-                  << total_time.count() << " ms" << std::endl;
-      }
-      return 0;
-    }
-    else if (preprocessing_success and !one_step_result.realizability.value()){
-      std::cout << Syft::UNREALIZABLE_STR << std::endl;
-      auto total_time = total_time_stopwatch.stop();
+        Syft::ExplicitStateDfaMona explicit_dfa_mona = Syft::ExplicitStateDfaMona::dfa_of_formula(*parsed_formula);
 
-      if (print_times) {
-        std::cout << "Total time: "
-                  << total_time.count() << " ms" << std::endl;
-      }
-      return 0;
-    }
-    else {
-      // Preprocessing was not successful. Continuing with full DFA construction."
-    }
+        Syft::ExplicitStateDfa explicit_dfa = Syft::ExplicitStateDfa::from_dfa_mona(var_mgr, explicit_dfa_mona);
 
-    Syft::ExplicitStateDfaMona explicit_dfa_mona = Syft::ExplicitStateDfaMona::dfa_of_formula(*parsed_formula);
+        Syft::SymbolicStateDfa symbolic_dfa = Syft::SymbolicStateDfa::from_explicit(
+                std::move(explicit_dfa));
 
-    Syft::ExplicitStateDfa explicit_dfa = Syft::ExplicitStateDfa::from_dfa_mona(var_mgr, explicit_dfa_mona);
-
-    Syft::SymbolicStateDfa symbolic_dfa = Syft::SymbolicStateDfa::from_explicit(
-            std::move(explicit_dfa));
-
-    auto aut_time = aut_time_stopwatch.stop();
-    if (print_times) {
-      std::cout << "DFA construction time: "
-                << aut_time.count() << " ms" << std::endl;
-    }
-
-    var_mgr->partition_variables(partition.input_variables,
-                                 partition.output_variables);
-
-    Syft::ReachabilitySynthesizer synthesizer(symbolic_dfa, starting_player,
-                                              protagonist_player, symbolic_dfa.final_states(),
-                                              var_mgr->cudd_mgr()->bddOne());
-    Syft::SynthesisResult result = synthesizer.run();
-
-    realizability = result.realizability;
-    if (realizability == true) {
-        std::cout << Syft::REALIZABLE_STR << std::endl;
-
-        Syft::Stopwatch abstract_single_strategy_time_stopwatch; // stopwatch for abstract single strategy
-        abstract_single_strategy_time_stopwatch.start();
-
-        auto transducer = synthesizer.AbstractSingleStrategy(std::move(result));
-        if (print_strategy){
-            transducer->dump_dot("strategy.dot");
-        }
-        auto abstract_single_strategy_time = abstract_single_strategy_time_stopwatch.stop();
+        auto aut_time = aut_time_stopwatch.stop();
         if (print_times) {
-          std::cout << "Abstract single strategy time: "
-                    << abstract_single_strategy_time.count() << " ms" << std::endl;
+            std::cout << "DFA construction time: "
+                      << aut_time.count() << " ms" << std::endl;
         }
-    }
-    else{
+
+        var_mgr->partition_variables(partition.input_variables,
+                                     partition.output_variables);
+
+        Syft::ReachabilitySynthesizer synthesizer(symbolic_dfa, starting_player,
+                                                  protagonist_player, symbolic_dfa.final_states(),
+                                                  var_mgr->cudd_mgr()->bddOne());
+        Syft::SynthesisResult result = synthesizer.run();
+
+        realizability = result.realizability;
+        if (realizability == true) {
+            std::cout << Syft::REALIZABLE_STR << std::endl;
+
+            Syft::Stopwatch abstract_single_strategy_time_stopwatch; // stopwatch for abstract single strategy
+            abstract_single_strategy_time_stopwatch.start();
+
+            auto transducer = synthesizer.AbstractSingleStrategy(std::move(result));
+            if (print_strategy) {
+                transducer->dump_dot("strategy.dot");
+            }
+            auto abstract_single_strategy_time = abstract_single_strategy_time_stopwatch.stop();
+            if (print_times) {
+                std::cout << "Abstract single strategy time: "
+                          << abstract_single_strategy_time.count() << " ms" << std::endl;
+            }
+        } else {
+            std::cout << Syft::UNREALIZABLE_STR << std::endl;
+        }
+    } else {
+        // abstract max strategy
+        Syft::ExplicitStateDfaMona explicit_dfa_mona = Syft::ExplicitStateDfaMona::dfa_of_formula(*parsed_formula);
+        Syft::ExplicitStateDfa explicit_dfa =  Syft::ExplicitStateDfa::from_dfa_mona(var_mgr, explicit_dfa_mona);
+
+        Syft::SymbolicStateDfa symbolic_dfa = Syft::SymbolicStateDfa::from_explicit(
+                std::move(explicit_dfa));
+
+        auto aut_time = aut_time_stopwatch.stop();
+        if (print_times) {
+            std::cout << "DFA construction time: "
+                      << aut_time.count() << " ms" << std::endl;
+        }
+
+        Syft::Stopwatch nondef_strategy_time_stopwatch; // stopwatch for strategy_generator construction
+        nondef_strategy_time_stopwatch.start();
+
+        var_mgr->partition_variables(partition.input_variables,
+                                     partition.output_variables);
+
+        Syft::ReachabilityMaxSetSynthesizer synthesizer(symbolic_dfa, starting_player,
+                                                        protagonist_player, symbolic_dfa.final_states(),
+                                                        var_mgr->cudd_mgr()->bddOne());
+        Syft::SynthesisResult result = synthesizer.run();
+
+        realizability = result.realizability;
+        if (realizability == true) {
+
+            auto nondef_strategy_time = nondef_strategy_time_stopwatch.stop();
+            if (print_times) {
+                std::cout << "Nondeferring strategy generator construction time: "
+                          << nondef_strategy_time.count() << " ms" << std::endl;
+            }
+
+            Syft::Stopwatch def_strategy_time_stopwatch; // stopwatch for abstract max-deferring strategy
+            def_strategy_time_stopwatch.start();
+
+            Syft::MaxSet maxset_strategy = synthesizer.AbstractMaxSet(std::move(result));
+
+            auto def_strategy_time = def_strategy_time_stopwatch.stop();
+            if (print_times) {
+                std::cout << "Deferring strategy generator construction time: "
+                          << def_strategy_time.count() << " ms" << std::endl;
+            }
+            std::cout << Syft::REALIZABLE_STR << std::endl;
+            if (print_strategy) {
+                synthesizer.dump_dot(maxset_strategy, "def_strategy.dot", "nondef_strategy.dot");
+            }
+        }
+        else{
         std::cout << Syft::UNREALIZABLE_STR << std::endl;
+        }
     }
 
   auto total_time = total_time_stopwatch.stop();
---
+++ src/synthesis/source/ReachabilityMaxSetSynthesizer.cpp
@@ -0,0 +1,77 @@
+//
+// Created by shuzhu on 20/01/24.
+//
+#include "ReachabilityMaxSetSynthesizer.h"
+
+#include <cassert>
+
+namespace Syft {
+
+    ReachabilityMaxSetSynthesizer::ReachabilityMaxSetSynthesizer(SymbolicStateDfa spec,
+                                                                 Player starting_player, Player protagonist_player,
+                                                                 CUDD::BDD goal_states,
+                                                                 CUDD::BDD state_space)
+            : DfaGameSynthesizer(spec, starting_player, protagonist_player)
+            , goal_states_(goal_states), state_space_(state_space)
+    {}
+
+
+    SynthesisResult ReachabilityMaxSetSynthesizer::run() const {
+        SynthesisResult result;
+        CUDD::BDD winning_states = state_space_ & goal_states_;
+        CUDD::BDD winning_moves = winning_states;
+
+        while (true) {
+            CUDD::BDD new_winning_states, new_winning_moves;
+
+            if (starting_player_ == Player::Agent){
+                CUDD::BDD quantified_X_transitions_to_winning_states = preimage(winning_states);
+                new_winning_moves = winning_moves |
+                                    (state_space_ & (!winning_states) & quantified_X_transitions_to_winning_states);
+
+                new_winning_states = project_into_states(new_winning_moves);
+            } else {
+                CUDD::BDD transitions_to_winning_states = preimage(winning_states);
+                CUDD::BDD new_collected_winning_states = project_into_states(transitions_to_winning_states);
+                new_winning_states = winning_states | new_collected_winning_states;
+                new_winning_moves = winning_moves |
+                                    ((!winning_states) & new_collected_winning_states & transitions_to_winning_states);
+            }
+
+            if (includes_initial_state(new_winning_states)) {
+                result.realizability = true;
+                result.winning_states = new_winning_states;
+                result.winning_moves = new_winning_moves;
+                result.transducer = nullptr;
+                return result;
+
+            } else if (new_winning_states == winning_states) {
+                result.realizability = false;
+                result.winning_states = new_winning_states;
+                result.winning_moves = new_winning_moves;
+                result.transducer = nullptr;
+                return result;
+            }
+
+            winning_moves = new_winning_moves;
+            winning_states = new_winning_states;
+        }
+
+    }
+
+
+    MaxSet ReachabilityMaxSetSynthesizer::AbstractMaxSet(SynthesisResult result) const {
+        MaxSet maxset;
+        maxset.nondeferring_strategy = result.winning_moves;
+        maxset.deferring_strategy = result.winning_moves | (result.winning_states & preimage(result.winning_states));
+        return maxset;
+    }
+
+    void ReachabilityMaxSetSynthesizer::dump_dot(MaxSet maxset, const std::string &def_filename,
+                                                 const std::string &nondef_filename) const {
+        var_mgr_->dump_dot(maxset.deferring_strategy.Add(), def_filename);
+        var_mgr_->dump_dot(maxset.nondeferring_strategy.Add(), nondef_filename);
+    }
+
+
+}
